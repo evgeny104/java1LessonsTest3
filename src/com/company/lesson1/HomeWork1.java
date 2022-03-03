abb20436584618e10cd7a06998ea21181d3014c5
evgeny104 <nazawka88@gmail.com>
2022-03-03 20:38:47 +0300

    Выполнил первое ДЗ

---
+++ src/com/company/lesson1/HomeWork1.java
@@ -0,0 +1,12 @@
+package com.company.lesson1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+
+    }
+
+    private static void task1() {
+        System.out.println("Рещение первой задачиЭ");
+
+    }
+}
